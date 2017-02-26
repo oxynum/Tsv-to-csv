568932bbf027cf0e9b61f0a0f9ddeab9a5158f15
Gasnier Maxime <maxime.gasnier.1@gmail.com>
2017-02-26 23:48:22 +0100

    configure server and add views

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 let express = require('express'),
     app     = express();
+    colors  = require('colors');
 
 const port = process.env.PORT || 9000;
 
-console.log('Welcome to one of the SimbaDev app.');
+console.log('Welcome to one of the SimbaDev app. Developed by Oxynum Team'.blue);
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
