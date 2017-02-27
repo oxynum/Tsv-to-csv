e77ef55f76afaeaa6801480d593a09448236fb06
Simbadev <maxime.gasnier.1@gmail.com>
2017-02-27 12:38:05 +0100

    Add doc to project

--- src/controllers/requestsController.js
+++ src/controllers/requestsController.js
@@ -3,22 +3,37 @@ let jsonfile = require('json-file'), //TODO: TO REFACTO with module exports
 
 
 /**
-*
-*
+* Will get the request located in the json file SQLrequest.json
+* @param {NoParam}
 */
 function getQueryJSON() {
   return file.get("request");
 }
 
+/**
+* Will get the request located in the json file SQLrequest.json
+* @param {string} newQuery represents the query enterred by the view
+*/
 function updateQueryJSON(newQuery) {
   addQueryToHistory(file.get("request"));
   file.set("request", newQuery);
-
+  file.writeSync();
+  return true;
 }
 
+/**
+* TODO NOT YET IMPLEMENTED
+*/
 function addQueryToHistory(query) {
-
+  console.log('ERROR NOT YEST IMPLEMENTED');
 }
+
+/**
+* requestsController module.
+* @module /src/controllers/requestsController
+* @param getQueryJSON
+* @param updateQueryJSON
+*/
 module.exports = {
   getQueryJSON: getQueryJSON,
   updateQueryJSON: updateQueryJSON
