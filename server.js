ff47b697a61161c2d1aa5b0a6253592e20dbecae
Gasnier Maxime <maxime.gasnier.1@gmail.com>
2017-02-27 01:14:28 +0100

    add bootstrap and style to index.ejs

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
+const port = process.env.PORT || 9000;
 let express = require('express'),
-    app     = express();
+    app     = express(),
     colors  = require('colors');
 
-const port = process.env.PORT || 9000;
-
-console.log('Welcome to one of the SimbaDev app. Developed by Oxynum Team'.blue);
+console.log('Welcome to one of the SimbaDev app.' + ' -> Developed by Oxynum Team'.blue);
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (request, response) => {
-  response.render('index');
+  response.render('index', {number: 4});
 });
 
 app.listen(port, () => {
   console.log('App running on: '.green + port);
+  console.log('To get started with this app, start by construct your own SQL request... '.yellow);
+  console.log('Then you can try to launch it and download the file.'.yellow);
 });
